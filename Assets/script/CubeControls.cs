9799334b77efdf0b11578dea07b595356d6d76de
lego4el <kapusta958@gmail.com>
2017-03-08 21:00:19 +0200

    first commit

---
+++ Assets/script/CubeControls.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeControls : MonoBehaviour {
+  
+	// Use this for initialization
+	void Start () {
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+
+            Vector3 position = this.transform.position;
+            position.x++;
+            transform.position = position;
+
+        }
+        if (Input.GetKey(KeyCode.V))
+        {
+            Vector3 position = transform.position;
+            position.x--;
+            transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.U))
+        { Vector3 position = transform.position;
+            position.z++;
+            transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.P))
+        {
+            Vector3 position = transform.position;
+            position.z--;
+            transform.position = position;
+        }
+            if (Input.GetKey(KeyCode.Space))
+            { Vector3 position = transform.position;
+                position.y++;
+                transform.position = position;
+
+            }
+        }
+    }
+
